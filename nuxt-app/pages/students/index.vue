3be8ecc30414de58732218d76b3cca48be48739d
Anton <69902586+ant0n7@users.noreply.github.com>
2022-06-21 00:26:34 +0200

    Try lazy fetch.

--- nuxt-app/pages/students/index.vue
+++ nuxt-app/pages/students/index.vue
@@ -1,3 +1,38 @@
+<template>
+  <div class="container">
+    <!-- <h1>Students <button class="btn btn-primary btn-md mb-2 ms-auto float-end" type="button">Create</button></h1> -->
+    <Heading buttonText="Create" buttonLink="/students/create">Students</Heading>
+
+    <div v-if="pending">
+      <Alert type="info">
+        Loading...
+      </Alert>
+    </div>
+
+    <div class="row" v-else>
+      <div
+        class="col-md-3 col-12 p-2"
+        v-for="student in students"
+        :key="student.id"
+      >
+        <Card
+          :title="student.username"
+          :subtitle="student.email"
+          :link="`/students/${student.id}`"
+          />
+      </div>
+    </div>
+
+    <div class="row" v-if="!students">
+      <div class="col-12">
+        <Alert type="warning" warning-icon>
+          No students found.
+        </Alert>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
 import Card1 from '~~/components/Card.vue';
 // const { data: students } = await useFetch(() => 'http://localhost:8080/api/user', {
@@ -12,44 +47,13 @@ import Card1 from '~~/components/Card.vue';
 // })
 
 // Fetch data from external API
-const { data: students } = await useFetch("http://localhost:8080/api/user/", {
+const { pending, data: students } = await useLazyFetch("http://localhost:8080/api/user/", {
   headers: {
     Authorization: `Basic ${btoa("andrin:klarer")}`,
   },
 });
+watch(students, (newStudents) => {
+  console.log("Students Updated");
+  console.log(JSON.stringify(newStudents));
+})
 </script>
-
-<template>
-  <div class="container">
-    <!-- <h1>Students <button class="btn btn-primary btn-md mb-2 ms-auto float-end" type="button">Create</button></h1> -->
-    <Heading buttonText="Create" buttonLink="/students/create">Students</Heading>
-
-    <div class="row">
-      <div class="col-md-3 col-12">
-        
-      </div>
-    </div>
-
-    <div class="row" v-if="students">
-      <div
-        class="col-md-3 col-12 p-2"
-        v-for="student in students"
-        :key="student.id"
-      >
-        <Card
-          :title="student.username"
-          :subtitle="student.email"
-          :link="`/students/${student.id}`"
-          />
-      </div>
-    </div>
-
-    <div class="row" v-else>
-      <div class="col-12">
-        <Alert type="warning" warning-icon>
-          No students found.
-        </Alert>
-      </div>
-    </div>
-  </div>
-</template>
