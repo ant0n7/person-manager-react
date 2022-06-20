d1f92ab50acdcde4122220991e290c07667a36a0
Anton <69902586+ant0n7@users.noreply.github.com>
2022-06-20 14:36:22 +0200

    Change a to NuxtLink tag.

--- nuxt-app/components/Navbar.vue
+++ nuxt-app/components/Navbar.vue
@@ -2,12 +2,12 @@
   <header
     class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"
   >
-    <a
+    <NuxtLink
       href="/"
       class="d-flex align-items-center col-md-3 ms-2 mb-2 mb-md-0 text-dark text-decoration-none"
     >
       <img src="/logo_books.png" height="32" alt="Logo">
-    </a>
+    </NuxtLink>
 
     <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
       <li><NuxtLink to="/" class="nav-link px-2 link-dark">Home</NuxtLink></li>
