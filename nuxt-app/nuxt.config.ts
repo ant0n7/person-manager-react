989a2a90ecf266f7d18f1efd2b15f8a1c563f55f
Anton <69902586+ant0n7@users.noreply.github.com>
2022-06-20 11:19:45 +0200

    Setup frontend with nuxt 3.

---
+++ nuxt-app/nuxt.config.ts
@@ -0,0 +1,21 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Student Manager',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Student Manager to manage students and their modules' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
+  css: [
+    '@/assets/css/main.scss'
+  ]
+})
